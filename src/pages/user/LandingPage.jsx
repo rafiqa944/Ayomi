4eb71e0f69ee549d36e0684e0307974ce2d8ea3d
rafiqa944 <rafiqanurc@gmail.com>
2024-11-28 12:15:24 +0800

    Deskripsi perubahan pada LandingPage.jsx

--- src/pages/user/LandingPage.jsx
+++ src/pages/user/LandingPage.jsx
@@ -9,6 +9,7 @@ import pottanaman from '../../assets/foto/pottanaman.png';
 import tas from '../../assets/foto/tas.png';
 import limbah from '../../assets/foto/limbah.png';
 import Footer from '../../Components/Footer';
+import { Link } from 'react-router-dom';
 
 const LandingPage = () => {
   return (
@@ -26,33 +27,42 @@ const LandingPage = () => {
         <h3>Jejak volunteer Ayomi dalam membawa kebaikan untuk komunitas dan lingkungan. <br />
           <span>Mari terus melangkah bersama menuju masa depan yang lebih baik!</span></h3>
             <div>
-            <div className="gallery-text">
-            <h2>  
-              <span>Sampah</span> <br />
-              <span>bisa jadi</span> <br />
-              <span>Kerajinan?</span>
-            </h2>
-              <button className="sampah-btn">Lihat Selengkapnya</button>
-            </div>
+              <div className="gallery-text">
+                <h2>  
+                  <span>Sampah</span> <br />
+                  <span>bisa jadi</span> <br />
+                  <span>Kerajinan?</span>
+                </h2>
+                  <button className="sampah-btn">Lihat Selengkapnya</button>
+              </div>
+
               <div className="gallery-item2-wrapper">
                   <img src={pottanaman} alt="foto pot" className="gallery-item2" />
-                  <div className="gallery-item2-title">Pot tanaman dari galon bekas</div>
+                  <div className="gallery-item2-title">
+                    Pot tanaman dari galon bekas
                   <a className="gallery-item2-link">Selengkapnya</a>
+                  </div>
               </div>
 
               <div className="gallery-item2-wrapper">
                 <img src={tas} alt="foto tas" className="gallery-item2" />
-                <div className="gallery-item2-title">Tas belanja dari tutup botol plastik</div>
+                <div className="gallery-item2-title">
+                  Tas belanja dari tutup botol plastik
                 <a className="gallery-item2-link">Selengkapnya</a>
+                </div>
               </div>
 
               <div className="gallery-item2-wrapper">
                 <img src={limbah} alt="foto limbah" className="gallery-item2" />
-                <div className="gallery-item2-title">Limbah kaca jadi kerajinan mosaik</div>
-                 <a className="gallery-item2-link">Selengkapnya</a>
-              </div>
+                <div className="gallery-item2-title">
+                    Limbah kaca jadi kerajinan mosaik
+                    <Link to="/JenisPlastik" className="gallery-item2-link"> {/* ini contoh */}
+                        Selengkapnya
+                    </Link>
+                </div>
             </div>
-        </div>
+            </div>
+      </div>
       <Footer/>
     </div>
   );
