05ecc3432a236da453ebad0ab13271ef0c1fef0f
unknown <nrmwnalsa@gmail.com>
2024-12-07 20:02:32 +0800

    data user dan update sidebar

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,28 +1,3 @@
-// // src/components/Sidebar.jsx
-// import React from "react";
-// import { Link } from "react-router-dom"; // Jika menggunakan React Router untuk navigasi
-// import './Sidebar.css'; // Mengimpor file CSS
-// import LogoP from '../assets/foto/LogoP.png';
-
-// const Sidebar = () => {
-//   return (
-//     <div className="sidebar">
-//       <Link to="/">
-//         <img src={LogoP} alt="Logo" className='LogoP' />
-//       </Link>
-//       <ul className="menu">
-//         <li className="menuItem"><Link to="/dashboard">Dashboard</Link></li>
-//         <li className="menuItem"><Link to="/profile">Tutorial</Link></li>
-//         <li className="menuItem"><Link to="/settings">Donate</Link></li>
-//         <li className="menuItem"><Link to="/settings">User</Link></li>
-//         <li className="menuItem"><Link to="/logout">Logout</Link></li>
-//       </ul>
-//     </div>
-//   );
-// };
-
-// export default Sidebar;
-
 // src/components/Sidebar.jsx
 import React from "react";
 import { Link, useLocation } from "react-router-dom"; // Menggunakan useLocation dari React Router
@@ -44,14 +19,15 @@ const Sidebar = () => {
         <li className={`menuItem ${isActive("/dashboard")}`}>
           <Link to="/dashboard">Dashboard</Link>
         </li>
-        <li className={`menuItem ${isActive("/tutorial")}`}>
+        <li className={`menuItem ${isActive("/admintutorial")}`}>
           <Link to="/admintutorial">Tutorial</Link>
         </li>
         <li className={`menuItem ${isActive("/donate")}`}>
           <Link to="/donate">Donate</Link>
         </li>
-        <li className={`menuItem ${isActive("/user")}`}>
-          <Link to="/user">User</Link>
+        {/* Ubah path dari "/user" ke "/datauser" */}
+        <li className={`menuItem ${isActive("/datauser")}`}>
+          <Link to="/datauser">User</Link>
         </li>
         <li className={`menuItem ${isActive("/logout")}`}>
           <Link to="/logout">Logout</Link>
